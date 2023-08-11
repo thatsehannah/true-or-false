b00d5991c7f76e68a67126968a08c98e1fc20c96
E. Hannah <echannah631@gmail.com>
2023-08-11 10:33:02 -0500

    created the necessary arrays for the game

--- Program.cs
+++ Program.cs
@@ -9,10 +9,11 @@ namespace TrueOrFalse
             // Do not edit these lines
             Console.WriteLine("Welcome to 'True or False?'\nPress Enter to begin:");
             string entry = Console.ReadLine();
-            Tools.SetUpInputStream(entry);
+            //Tools.SetUpInputStream(entry);
 
-            // Type your code below
-
+            string[] questions = { "LeBron James has 4 championship rings.", "Inglewood and Los Angeles are two different cities in California.", "Barack Obama only served as president for one term.", "Intermittent fasting is a great way to gain weight.", "Meta owns Facebook and Instagram." };
+            bool[] answers = { true, true, false, false, true };
+            bool[] responses = new bool[answers.Length];
         }
     }
 }
