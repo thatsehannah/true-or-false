153922c00696855a5440f0798d0f4fd413741fac
E. Hannah <echannah631@gmail.com>
2023-08-11 11:13:45 -0500

    refactoring

--- Program.cs
+++ Program.cs
@@ -8,43 +8,58 @@ namespace TrueOrFalse
         {
             // Do not edit these lines
             Console.WriteLine("Welcome to 'True or False?'\nPress Enter to begin:");
-            string entry = Console.ReadLine();
+            string? entry = Console.ReadLine();
             //Tools.SetUpInputStream(entry);
 
             string[] questions = { "LeBron James has 4 championship rings.", "Inglewood and Los Angeles are two different cities in California.", "Barack Obama only served as president for one term.", "Intermittent fasting is a great way to gain weight.", "Meta owns Facebook and Instagram." };
             bool[] answers = { true, true, false, false, true };
-            bool[] responses = new bool[answers.Length];
 
-            if (questions.Length != answers.Length)
-            {
-                Console.WriteLine("WARNING: the length of the questions array and the length of the answers array are NOT equal");
-            }
+            RunQuiz(questions, answers);
+        }
+
+        static void RunQuiz(string[] questions, bool[] answers)
+        {
+            bool[] responses = AskQuestions(questions);
+            int score = ScoreResponses(answers, responses);
+
+            Console.WriteLine($"You got {score} out of {questions.Length} correct!");
+        }
+
+        static bool[] AskQuestions(string[] questions)
+        {
+            bool[] responses = new bool[questions.Length];
 
             int askingIndex = 0;
             foreach (string question in questions)
             {
-                string input;
+                string? input;
                 bool isBool;
                 bool inputBool;
                 Console.WriteLine(question);
                 Console.Write("True or False? ");
                 input = Console.ReadLine();
 
-                isBool = Boolean.TryParse(input, out inputBool);
+                isBool = bool.TryParse(input, out inputBool);
 
-                while(!isBool)
+                while (!isBool)
                 {
                     Console.WriteLine("Please respond with 'true' or 'false'.");
                     input = Console.ReadLine();
-                    isBool = Boolean.TryParse(input, out inputBool);
+                    isBool = bool.TryParse(input, out inputBool);
                 }
 
                 responses[askingIndex] = inputBool;
                 askingIndex++;
             }
 
-            int scoringIndex = 0;
+            return responses;
+        }
+
+        static int ScoreResponses(bool[] answers, bool[] responses)
+        {
             int score = 0;
+            int scoringIndex = 0;
+
             foreach (bool answer in answers)
             {
                 bool response = responses[scoringIndex];
@@ -56,7 +71,8 @@ namespace TrueOrFalse
                 }
                 scoringIndex++;
             }
-            Console.WriteLine($"You got {score} out of {questions.Length} correct!");
+
+            return score;
         }
     }
 }
