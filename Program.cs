ff5bb021851ff73b41e5e5ce4700b525b62ebc8a
E. Hannah <echannah631@gmail.com>
2023-08-11 10:49:28 -0500

    completed asking and capturing responses

--- Program.cs
+++ Program.cs
@@ -14,6 +14,35 @@ namespace TrueOrFalse
             string[] questions = { "LeBron James has 4 championship rings.", "Inglewood and Los Angeles are two different cities in California.", "Barack Obama only served as president for one term.", "Intermittent fasting is a great way to gain weight.", "Meta owns Facebook and Instagram." };
             bool[] answers = { true, true, false, false, true };
             bool[] responses = new bool[answers.Length];
+
+            if (questions.Length != answers.Length)
+            {
+                Console.WriteLine("WARNING: the length of the questions array and the length of the answers array are NOT equal");
+            }
+
+            int askingIndex = 0;
+            foreach (string question in questions)
+            {
+                string input;
+                bool isBool;
+                bool inputBool;
+                Console.WriteLine(question);
+                Console.Write("True or False? ");
+                input = Console.ReadLine();
+
+                isBool = Boolean.TryParse(input, out inputBool);
+
+                while(!isBool)
+                {
+                    Console.WriteLine("Please respond with 'true' or 'false'.");
+                    input = Console.ReadLine();
+                    isBool = Boolean.TryParse(input, out inputBool);
+                }
+
+                responses[askingIndex] = inputBool;
+                askingIndex++;
+            }
+            
         }
     }
 }
